e6d803c879ee786be4474e3594090823c5b9455e
itchenfei <itchenfei@gmail.com>
2023-05-15 22:45:30 +0800

    init test

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,58 +1,48 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div v-if="!connected">
+      <input v-model="username" placeholder="Username" />
+      <input v-model="password" type="password" placeholder="Password" />
+      <input v-model="hostname" placeholder="Hostname" />
+      <button @click="connect">Connect</button>
+    </div>
+    <div v-else>
+      <textarea v-model="output" readonly></textarea>
+      <input v-model="input" @keyup.enter="send" placeholder="Enter command" />
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import io from 'socket.io-client';
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      hostname: '',
+      connected: false,
+      output: '',
+      input: '',
+      socket: null,
+    };
+  },
+  methods: {
+    connect() {
+      this.socket = io('http://localhost:8000');
+      this.socket.on('connect', () => {
+        this.connected = true;
+        this.socket.emit('message', `${this.username}:${this.password}:${this.hostname}`);
+      });
+      this.socket.on('message', (data) => {
+        this.output += data;
+      });
+    },
+    send() {
+      this.socket.emit('message', this.input);
+      this.input = '';
+    },
+  },
+};
+</script>
